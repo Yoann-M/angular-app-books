7def91344d4baeba432c898ec0ad76532dfcec05
Yoann M <Yoann-M@users.noreply.github.com>
2018-10-03 11:18:59 +0200

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BooksComponent } from './components/books/books.component';
+import { BooksDetailComponent } from './components/books/books-detail/books-detail.component';
+import { BooksFormComponent } from './components/books/books-form/books-form.component';
+import { AuthSigninComponent } from './components/auth/auth-signin/auth-signin.component';
+import { AuthSignupComponent } from './components/auth/auth-signup/auth-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BooksComponent,
+    BooksDetailComponent,
+    BooksFormComponent,
+    AuthSigninComponent,
+    AuthSignupComponent,
   ],
   imports: [
     BrowserModule
